d25a7aa44504c76314578789693d7a752b216229
Krishna Gupta <ishanbeast662@gmail.com>
2026-03-29 18:35:41 +0530

    initialized the project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-100 text-white">
+      <h1 className="text-5xl font-bold text-green-400">RupeeRadar 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
